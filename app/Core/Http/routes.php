138847aac4b2d8b451490068a9bdf1a1a84bbf92
Vinicius Reis <luiz.vinicius73@gmail.com>
2015-07-20 22:49:59 -0300

    extend Application

--- app/Core/Http/routes.php
+++ app/Core/Http/routes.php
@@ -12,5 +12,5 @@
 */
 
 Route::get('/', function () {
-    return dd(app());
+    return view('welcome');
 });
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,7 @@
 |
 */
 
-$app = new Illuminate\Foundation\Application(
+$app = new App\Core\Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
 );
 
