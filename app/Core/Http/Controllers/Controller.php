2833a5d2a598bec10865870579b3f7b0b03507ff
Vinicius Reis <luiz.vinicius73@gmail.com>
2015-07-31 23:22:03 -0300

    up

--- app/Core/Http/Controllers/Controller.php
+++ app/Core/Http/Controllers/Controller.php
@@ -10,4 +10,16 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * @param string $view
+     * @param array  $data
+     * @param array  $mergeData
+     *
+     * @return \Illuminate\View\View
+     */
+    protected function view($view = null, $data = [], $mergeData = [])
+    {
+        return view($view, $data, $mergeData);
+    }
 }
