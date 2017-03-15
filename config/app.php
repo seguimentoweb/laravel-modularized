1ccdf9d22ef044a00a44ed250a4364f581ef3ec2
Euller Cristian <eullercdr@gmail.com>
2017-03-15 00:29:09 -0300

    Add useful packages

--- config/app.php
+++ config/app.php
@@ -138,6 +138,20 @@ return [
         Illuminate\View\ViewServiceProvider::class,
 
         /*
+         * Package Service Providers...
+         */
+        Barryvdh\Debugbar\ServiceProvider::class,
+        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
+        Prettus\Repository\Providers\RepositoryServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
+        OwenIt\Auditing\AuditingServiceProvider::class,
+        Artesaos\Defender\Providers\DefenderServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
+        KennedyTedesco\Validation\ValidationServiceProvider::class,
+
+
+        /*
          * Application Service Providers...
          */
         App\Core\Providers\CoreServiceProvider::class,
@@ -196,6 +210,11 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        'Defender' => Artesaos\Defender\Facades\Defender::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class
 
     ],
 
